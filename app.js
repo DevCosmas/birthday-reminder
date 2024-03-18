ec2d5231da33edef6a6ab1070e7f423350f6e382
Ugwucosmas Chibuike <buikem0112@gmail.com>
2024-03-18 20:46:26 +0100

    added a 404 error page

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/', viewRouter);
 
 SendCronJob();
 app.all('*', (req, res, next) => {
-  res.status(404).json({ status: 'fail', message: 'page not found' });
+  res.status(404).render('404');
 });
 app.use(errorhandler);
 app.listen(PORT, () => {
--- routes/view.js
+++ routes/view.js
@@ -4,4 +4,7 @@ const viewRouter = express.Router();
 viewRouter.get('/', (req, res) => {
   res.status(200).render('indexView');
 });
+viewRouter.get('*', (req, res) => {
+  res.status(404).render('404');
+});
 module.exports = viewRouter;
